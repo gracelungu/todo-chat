1c3739346bbf11b60a6f0d9fe95e216324c6f88f
Grace Lungu <grace.lungu.bw@gmail.com>
2023-11-20 21:38:57 +0100

    Add initial index page

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Todo Chat Application</h1>
+    <p>This is the home page of the application.</p>
+  </div>
+);
+
+export default home;
